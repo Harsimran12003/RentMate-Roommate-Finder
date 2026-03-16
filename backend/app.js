11018a0bf74213538499d895b47cc71b1544b413
Harsimran12003 <harsimran12003@gmail.com>
2026-03-16 16:52:40 +0530

    added profile photo4

--- backend/app.js
+++ backend/app.js
@@ -37,12 +37,11 @@ socketHandler(server);
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    dbName: "rentmate",
+    serverSelectionTimeoutMS: 30000,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
 // Routes
 
 app.use("/api/users", userRoutes);
